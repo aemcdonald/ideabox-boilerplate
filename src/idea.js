6e529c3582490cc3b481d40b4640301d56306d06
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-07-24 20:42:56 -0600

    Add ID to class

--- src/idea.js
+++ src/idea.js
@@ -1,6 +1,6 @@
 class Idea {
-  constructor(id, title, body, star) {
-    this.id = id;
+  constructor(title, body, star) {
+    this.id = Date.now(); //accesses Date object which has a .now method that takes this very instance in time;
     this.title = title;
     this.body = body;
     this.star = star
